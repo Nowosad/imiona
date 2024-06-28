d813dbd61d63434c483a0ddd08bf452fce53cee9
Jakub Nowosad <tupiszakaczki@gmail.com>
2024-06-28 16:31:33 +0200

    updates

--- namer/app.R
+++ namer/app.R
@@ -1,7 +1,18 @@
-library(shiny)
-library(ggplot2)
-library(dplyr)
-load("babynamespl.rda")
+
+# load("namer/babynamespl.rda")
+
+# babynamespl = structure(list(Rok = c(2000, 2000, 2000, 2000, 2000, 2000, 2000,
+# 2000, 2000, 2000), Imię = c("JAKUB", "MATEUSZ", "KACPER", "MICHAŁ",
+# "PATRYK", "DAWID", "KAMIL", "PIOTR", "SZYMON", "PAWEŁ"), Liczba = c(14497,
+# 11454, 8898, 8498, 7743, 7063, 6842, 5608, 5486, 5109), Płeć = c("M",
+# "M", "M", "M", "M", "M", "M", "M", "M", "M")), row.names = c(NA,
+# -10L), spec = structure(list(cols = list(Rok = structure(list(), class = c("collector_double",
+# "collector")), Imię = structure(list(), class = c("collector_character",
+# "collector")), Liczba = structure(list(), class = c("collector_double",
+# "collector")), Płeć = structure(list(), class = c("collector_character",
+# "collector"))), default = structure(list(), class = c("collector_guess",
+# "collector")), delim = ","), class = "col_spec"), class = c("spec_tbl_df",
+# "tbl_df", "tbl", "data.frame"))
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -29,6 +40,14 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  library(shiny)
+  library(ggplot2)
+  library(dplyr)
+
+  load(url("https://github.com/Nowosad/imiona/raw/main/babynamespl.rda"))
+  # babynamespl = read.csv("https://github.com/Nowosad/imiona/raw/main/babynames.csv")
+
+  babynamespl = babynamespl[1:100,]
 
     output$distPlot <- renderPlot({
         ggplot(filter(babynamespl, Imię %in% input$imie),
