ee86c8add194342183007ce6934336a31ab18e3b
Jakub Nowosad <tupiszakaczki@gmail.com>
2024-06-29 07:50:19 +0200

    cleans app

--- namer/app.R
+++ namer/app.R
@@ -3,40 +3,31 @@ library(ggplot2)
 
 load(url("https://raw.githubusercontent.com/Nowosad/imiona/main/babynamespl.RData"))
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+ui = fluidPage(
+  titlePanel("Imiona"),
 
-    # Application title
-    titlePanel(""),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            selectizeInput(inputId = "imie",
-                           label = "Imię",
-                           choices = babynamespl$Imię,
-                           selected = c("EWA", "DAWID"),
-                           multiple = TRUE,
-                           options = NULL)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  sidebarLayout(
+    sidebarPanel(
+    selectizeInput(
+      inputId = "imie",
+      label = "Imię",
+      choices = babynamespl$Imię,
+      selected = c("EWA", "DAWID"),
+      multiple = TRUE,
+      options = NULL
     )
+  ),
+  mainPanel(plotOutput("distPlot"))
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        ggplot(subset(babynamespl, Imię %in% input$imie),
-               aes(Rok, Liczba, color = Imię, group = Imię)) +
-            geom_line() +
-            geom_point()
-    })
+server = function(input, output) {
+  output$distPlot = renderPlot({
+    ggplot(subset(babynamespl, Imię %in% input$imie),
+           aes(Rok, Liczba, color = Imię, group = Imię)) +
+      geom_line() +
+      geom_point()
+  })
 }
 
-# Run the application
 shinyApp(ui = ui, server = server)
